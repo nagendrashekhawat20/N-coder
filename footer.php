0fbc09b7f7e0a59208c5fa5ea2b2599698917f1f
Surya978 <suryapratap.8march@gmail.com>
2023-08-31 20:04:25 +0530

    s

---
+++ footer.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Top Master Services</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/fav.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/custom-animation.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/venobox.min.css">
+    <link rel="stylesheet" href="assets/css/backToTop.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.css">
+    <link rel="stylesheet" href="assets/css/default.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+    <footer class="theme-dark-bg2">
+        <div class="tp-footer-area-two">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-md-3 mb-10 foot-new">
+                        <img src="./assets/img/footer/footer-1 2.png" alt="Image not found" class="img-fluid">
+                    </div>
+                    <div class="col-12 col-md-2">
+                        <h4>
+                            Subscribe to Newsletter
+                        </h4>
+                    </div>
+                    <div class="col-12 col-md-5 mb-4">
+                        <form action="#">
+                            <label for="email" class="form-label"></label>
+                            <input type="email" class="form-control" name="email" id="email" aria-describedby="email"
+                                placeholder="Enter your email address" style="border: 0.5px solid #cfceda73; color: #A5C766; background-color: #ffffff00;">
+                        </form>
+                    </div>
+                    <div class="col-12 col-md-2 mb-3">
+                        <button type="submit" class="footer-btn btn">Submit Now</button>
+                    </div>
+                </div>
+
+            </div>
+            <hr class="mt-0">
+            <div class="container">
+                <div class="footer-bubble">
+                    <img src="./assets/img/footer/bubble1.png" alt="Image not found" class="img-fluid">
+                </div>
+                <div class="row pb-40 pt-40">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="tp-footer-widget footer-col-1 mb-30 wow fadeInUp" data-wow-delay=".3s">
+                            <div class="tp-footer-info tp-footer-info-three">
+                                <div class="tp-footer-info-logo mb-35">
+                                    <a href="index.html"><img src="assets/img/logo/logo-footer.png" class="img-fluid"
+                                            alt="img not found"></a>
+                                </div>
+                                <!-- <p class="text-white mb-35">Pulvinar seneius morbi to quisque nunc a towa faucibus netus a mone lestie nice disease</p> -->
+                                <div class="tp-footer-info-social tp-footer-info-social-three">
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="tp-footer-widget footer-col-2 mb-30 wow fadeInUp" data-wow-delay=".6s">
+                            <h4 class="tp-footer-widget-title mb-35">Info</h4>
+                            <ul>
+                                <li><a href="./index-3.html">Home</a></li>
+                                <li><a href="./about.html">About Us</a></li>
+                                <li><a href="./Guide.html">Guides</a></li>
+                                <li><a href=" " data-bs-target="#modalContactForm" data-bs-toggle="modal">Get a quote</a></li>
+                                <li><a href="./contact.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="tp-footer-widget mb-30 wow fadeInUp" data-wow-delay=".9s">
+                            <h4 class="tp-footer-widget-title mb-35">Services</h4>
+                            <ul>
+                                <li><a href="./Residential _Cleaning.html">Residential Cleaning</a></li>
+                                <li><a href="./Office-cleaning.html">Office Cleaning</a></li>
+                                <li><a href="./Emergency-Cleaning.html">Emergency Cleaning</a></li>
+                                <li><a href="./Gardening & Landscaping.html">Gardening & Landscaping</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="tp-footer-widget footer-col-4 mb-30 wow fadeInUp" data-wow-delay="1.2s">
+                            <h4 class="tp-footer-widget-title mb-40">Contact</h4>
+                            <ul>
+                                <li class="text-white"><b>Email Us:</b></li>
+                                <li><a href="mail: ">admin@topmasterservice.com.au</a></li>
+                                <li><a href="tel: 0481 826 682">0481 826 682</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <hr class="text-light">
+            </div>
+        </div>
+        <div class="tp-copyright-area-two theme-dark-bg3 z-index pt-30 pb-30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="tp-copyright tp-copyright-two">
+                            <p class="m-0">© Copyright Top Master Service 2023 . All right reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="modal fade" id="modalContactForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+
+                <div class="modal-body">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-md-4 pop-up-img mt-20">
+                            <img src="./assets/img/about/pop-up.png" alt="Image not found" class="img-fluid">
+                        </div>
+                        <div class="col-12 col-md-8 pop-up-detail pb-20">
+                            <div class="modal-header text-center">
+                                <h2 class="modal-title">Request a free quote</h2>
+                                <button type="button" id="Close_popup" class="close text-dark" data-bs-dismiss="modal"
+                                    aria-label="Close">
+                                    <span aria-hidden="true"><i class="fa fa-close"></i></span>
+                                </button>
+                            </div>
+                            <div class="row form-m-inline">
+                                <div class="form-group col-12 col-sm-6 col-md-6">
+                                    <label for="firstName"></label>
+                                    <input type="text" class="form-control" id="firstName" name="name"
+                                        placeholder="First Name" />
+                                </div>
+                                <div class="form-group col-12 col-sm-6 col-md-6">
+                                    <label for="lastName"></label>
+                                    <input type="text" class="form-control" id="lastName" name="name"
+                                        placeholder="Last Name" />
+                                </div>
+                            </div>
+
+                            <div class="row form-m-inline">
+                                <div class="form-group col-12 col-sm-6 col-md-6">
+                                    <label for="email"></label>
+                                    <input type="text" class="form-control" id="email" name="email"
+                                        placeholder="Email address" />
+                                </div>
+
+                                <div class="form-group col-12 col-sm-6 col-md-6">
+                                    <label for="phone"></label>
+                                    <input type="number" class="form-control" id="phone" name="phone"
+                                        placeholder="Phone" />
+                                </div>
+                            </div>
+
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Select Services</option>
+                                <option value="1"></option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+
+                            <div class="form-group">
+                                <label for="message"></label>
+                                <textarea class="form-control" id="message" name="message" rows="4"
+                                    placeholder="Message"></textarea>
+                            </div>
+
+                            <a type="submit" class="btn contact-form col-12">
+                                Submit
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- JS here -->
+    <script src="assets/js/vendor/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/swiper-bundle.js"></script>
+    <script src="assets/js/venobox.min.js"></script>
+    <script src="assets/js/backToTop.js"></script>
+    <script src="assets/js/jquery.meanmenu.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/ajax-form.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <script>
+        function openModal() {
+            $('#modalContactForm').modal('show');
+        }
+
+        var imagePopUp = document.querySelector();
+        imagePopUp.addEventListener('click', function (e) {
+            $('#modalContactForm').modal('show');
+        });
+    </script>
+</body>
+
+</html>
