73fb9e3be9358d522f7e6d7d9c79aba5a7abdd21
Surya978 <suryapratap.8march@gmail.com>
2023-08-31 19:51:39 +0530

    s

---
+++ header.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Top Master Services</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/fav.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/custom-animation.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/venobox.min.css">
+    <link rel="stylesheet" href="assets/css/backToTop.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.css">
+    <link rel="stylesheet" href="assets/css/default.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+
+
+    <!-- preloader -->
+    <div id="preloader">
+        <div class="preloader">
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <!-- preloader end  -->
+
+    <!-- back to top start -->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
+    <!-- back to top end -->
+
+    <section class="info-welcome">
+        <div class="container">
+            <p>
+                Welcome to Top Masters - Your Cleaning Wizards!
+            </p>
+        </div>
+    </section>
+
+    <header>
+        <div class="tp-header-area-three header-sticky">
+            <div class="tp-custom-container">
+                <div class="row justify-content-xl-center align-items-center">
+                    <div class="col-xxl-3 col-xl-4 col-8">
+                        <div class="tp-header-logo-three">
+                            <a href="index.html"><img src="assets/img/logo/logo.png" class="img-fluid"
+                                    alt="logo not found"></a>
+                        </div>
+                    </div>
+                    <div class="col-xxl-6 col-xl-6 col-4 d-flex justify-content-center">
+                        <div class="tp-main-menu tp-main-menu-three">
+                            <nav id="tp-mobile-menu">
+                                <ul>
+                                    <li class="menu-item-has-children"><a href="index.html" class="active">HOME</a>
+
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="about.html">ABOUT</a>
+
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="service.html">SERVICES</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="./Office-cleaning.html">Office Cleaning</a></li>
+                                            <li><a href="./Residential _Cleaning.html">Residential Cleaning</a></li>
+                                            <li><a href="./Gardening & Landscaping.html">Gardening & Landscaping</a>
+                                            </li>
+                                            <li><a href="./Emergemcy-Cleaning">Emergemcy Cleaning</a></li>
+                                            <li><a href="./Carpet-Steam-Clean.html">Carpet Steam Clean</a></li>
+                                            <li><a href="./High-Pressure-Cleaning.html">High Pressure Cleaning</a></li>
+                                            <li><a href="./Strip-&-Seal.html">Strip & Seal</a></li>
+                                            <li><a href="./Window-Cleaning.html">Window Cleaning</a></li>
+                                            <li><a href="./High-Level-Window-Cleaning.html">High Level Window
+                                                    Cleaning</a></li>
+                                            <li><a href="./Graffiti-Removal.html">Graffiti Removal</a></li>
+                                            <li><a href="./Hard-Floor-Scrubbing.html">Hard Floor Scrubbing</a></li>
+                                            <li><a href="./House-moves.html">House Moves</a></li>
+                                            <li><a href="./Labour-hire.html">Labor Hire</a></li>
+
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="guide.html">GUIDES</a>
+
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="contact.html">CONTACT</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <!-- mobile menu activation -->
+                        <div class="side-menu-icon d-xl-none text-end">
+                            <button class="side-toggle text-dark"><i class="far fa-bars"></i></button>
+                        </div>
+                    </div>
+                    <div class="col-xxl-3 col-xl-2 d-none d-xl-block">
+                        <div class="tp-header-right-three">
+                            <!-- <div class="tp-header-number-three">
+                                <span>Call Us : <a href="tel:33888200-55">33 888 200 - 55</a></span>
+                            </div> -->
+                            <div class="tp-header-btn-three">
+                                <a href="tel:33888200-55" class="header-btn"><b>Call Us:</b>0481 826 682</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- mobile menu info -->
+    <div class="fix">
+        <div class="side-info">
+            <button class="side-info-close"><i class="fal fa-times"></i></button>
+            <div class="side-info-content">
+                <div class="tp-mobile-menu"></div>
+                <div class="contact-infos mb-30">
+                    <div class="contact-list mb-30">
+                        <h4>Contact Info</h4>
+                        <ul>
+                            <li><i class="flaticon-pin"></i>54B, Tailstoi Town 5238 ST,La city, IA 522364</li>
+                            <li><i class="flaticon-email"></i><a
+                                    href="mailto:info@klenar.com">admin@topmasterservice.com.au</a></li>
+                            <li><i class="flaticon-phone-call"></i><a href="tel:0481 826 682">0481 826 682</a></li>
+                        </ul>
+                        <div class="sidebar__menu--social">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-google"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="offcanvas-overlay"></div>
+    <!-- mobile menu info -->
+    <!-- JS here -->
+    <script src="assets/js/vendor/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/swiper-bundle.js"></script>
+    <script src="assets/js/venobox.min.js"></script>
+    <script src="assets/js/backToTop.js"></script>
+    <script src="assets/js/jquery.meanmenu.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/ajax-form.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <script>
+        function openModal() {
+            $('#modalContactForm').modal('show');
+        }
+
+        var imagePopUp = document.querySelector();
+        imagePopUp.addEventListener('click', function (e) {
+            $('#modalContactForm').modal('show');
+        });
+    </script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -68,103 +68,10 @@
         </div>
     </section>
 
-    <header>
-        <div class="tp-header-area-three header-sticky">
-            <div class="tp-custom-container">
-                <div class="row justify-content-xl-center align-items-center">
-                    <div class="col-xxl-3 col-xl-4 col-8">
-                        <div class="tp-header-logo-three">
-                            <a href="index.html"><img src="assets/img/logo/logo.png" class="img-fluid"
-                                    alt="logo not found"></a>
-                        </div>
-                    </div>
-                    <div class="col-xxl-6 col-xl-6 col-4 d-flex justify-content-center">
-                        <div class="tp-main-menu tp-main-menu-three">
-                            <nav id="tp-mobile-menu">
-                                <ul>
-                                    <li class="menu-item-has-children"><a href="index.html" class="active">HOME</a>
 
-                                    </li>
-                                    <li class="menu-item-has-children"><a href="about.html">ABOUT</a>
-
-                                    </li>
-                                    <li class="menu-item-has-children"><a href="service.html">SERVICES</a>
-                                        <ul class="sub-menu">
-                                            <li><a href="./Office-cleaning.html">Office Cleaning</a></li>
-                                            <li><a href="./Residential _Cleaning.html">Residential Cleaning</a></li>
-                                            <li><a href="./Gardening & Landscaping.html">Gardening & Landscaping</a>
-                                            </li>
-                                            <li><a href="./Emergemcy-Cleaning">Emergemcy Cleaning</a></li>
-                                            <li><a href="./Carpet-Steam-Clean.html">Carpet Steam Clean</a></li>
-                                            <li><a href="./High-Pressure-Cleaning.html">High Pressure Cleaning</a></li>
-                                            <li><a href="./Strip-&-Seal.html">Strip & Seal</a></li>
-                                            <li><a href="./Window-Cleaning.html">Window Cleaning</a></li>
-                                            <li><a href="./High-Level-Window-Cleaning.html">High Level Window
-                                                    Cleaning</a></li>
-                                            <li><a href="./Graffiti-Removal.html">Graffiti Removal</a></li>
-                                            <li><a href="./Hard-Floor-Scrubbing.html">Hard Floor Scrubbing</a></li>
-                                            <li><a href="./House-moves.html">House Moves</a></li>
-                                            <li><a href="./Labour-hire.html">Labor Hire</a></li>
-
-                                        </ul>
-                                    </li>
-                                    <li class="menu-item-has-children"><a href="guide.html">GUIDES</a>
-
-                                    </li>
-                                    <li class="menu-item-has-children"><a href="contact.html">CONTACT</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <!-- mobile menu activation -->
-                        <div class="side-menu-icon d-xl-none text-end">
-                            <button class="side-toggle text-dark"><i class="far fa-bars"></i></button>
-                        </div>
-                    </div>
-                    <div class="col-xxl-3 col-xl-2 d-none d-xl-block">
-                        <div class="tp-header-right-three">
-                            <!-- <div class="tp-header-number-three">
-                                <span>Call Us : <a href="tel:33888200-55">33 888 200 - 55</a></span>
-                            </div> -->
-                            <div class="tp-header-btn-three">
-                                <a href="tel:33888200-55" class="header-btn"><b>Call Us:</b>0481 826 682</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- mobile menu info -->
-    <div class="fix">
-        <div class="side-info">
-            <button class="side-info-close"><i class="fal fa-times"></i></button>
-            <div class="side-info-content">
-                <div class="tp-mobile-menu"></div>
-                <div class="contact-infos mb-30">
-                    <div class="contact-list mb-30">
-                        <h4>Contact Info</h4>
-                        <ul>
-                            <li><i class="flaticon-pin"></i>54B, Tailstoi Town 5238 ST,La city, IA 522364</li>
-                            <li><i class="flaticon-email"></i><a
-                                    href="mailto:info@klenar.com">admin@topmasterservice.com.au</a></li>
-                            <li><i class="flaticon-phone-call"></i><a href="tel:0481 826 682">0481 826 682</a></li>
-                        </ul>
-                        <div class="sidebar__menu--social">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                            <a href="#"><i class="fab fa-google"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="offcanvas-overlay"></div>
-    <!-- mobile menu info -->
-
+    <?php
+include('header.php');
+?>
     <main>
         <!-- hero area start here -->
         <section class="tp-slider-area-three fix">
@@ -1024,7 +931,7 @@
                             <h4 class="tp-footer-widget-title mb-40">Contact</h4>
                             <ul>
                                 <li class="text-white"><b>Email Us:</b></li>
-                                <li><a href="mail: admin@topmasterservice.com.au">admin@topmasterservice.com.au</a></li>
+                                <li><a href="mail: ">admin@topmasterservice.com.au</a></li>
                                 <li><a href="tel: 0481 826 682">0481 826 682</a></li>
                             </ul>
                         </div>
