c597a8a1f02dee3801553f2f2e56945b80cde8c2
Surya978 <suryapratap.8march@gmail.com>
2023-08-31 20:27:10 +0530

    s

--- header.php
+++ header.php
@@ -43,22 +43,6 @@
 <body>
 
 
-    <!-- preloader -->
-    <div id="preloader">
-        <div class="preloader">
-            <span></span>
-            <span></span>
-        </div>
-    </div>
-    <!-- preloader end  -->
-
-    <!-- back to top start -->
-    <div class="progress-wrap">
-        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
-        </svg>
-    </div>
-    <!-- back to top end -->
 
     <section class="info-welcome">
         <div class="container">
